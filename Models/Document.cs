15313d4e22855cf4f14bb1fb6ced4fb80d71ec68
Samkelo  Tshabalala <101411280+SamTheCopy-ninja@users.noreply.github.com>
2024-11-09 16:36:59 +0200

    Transfer models and views for admin controller

---
+++ Models/Document.cs
@@ -0,0 +1,17 @@
+﻿namespace BumbleBeeFoundation_Client.Models
+{
+    public class Document
+    {
+        public int DocumentID { get; set; }
+        public string DocumentName { get; set; }
+        public string DocumentType { get; set; }
+        public DateTime UploadDate { get; set; }
+        public string Status { get; set; }
+        public string CompanyName { get; set; }
+
+        public int CompanyID { get; set; }
+
+        public byte[] FileContent { get; set; } // New field to store the file content
+        public int FundingRequestID { get; set; } // Link to FundingRequest
+    }
+}
